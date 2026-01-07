be2e82759ee2335a1e107c03e2b651c217846904
TheGuardianBaby <5cgqw524bj@privaterelay.appleid.com>
2026-01-07 15:14:18 +0300

    v 1.1

--- pip_scan.py
+++ pip_scan.py
@@ -2,15 +2,27 @@ import sys
 import subprocess
 import os
 import shutil
+import requests
 from scan import run_analize
+
+def check_ollama():
+    try:
+        response = requests.get("http://localhost:11434/api/tags", timeout=1)
+        if response.status_code != 200:
+            raise ConnectionError
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        print("ОШИБКА: Сервис Ollama не обнаружен!")
+        sys.exit(1)
 
 def main():
     args = sys.argv[1:]
     if "install" in args:
         packages1 = [a for a in args if not a.startswith("-") and a != "install"]
         if packages1:
+            check_ollama()
+            
             now_package = packages1[0]
-            print(f"LLM-Scan: Проверка пакета '{now_package}'")
+            print(f"Проверка пакета '{now_package}'")
             
             sandbox_temp = os.path.abspath("sandbox")
             if os.path.exists(sandbox_temp):
@@ -26,13 +38,13 @@ def main():
                 is_safe = run_analize(sandbox_temp)
                 
                 if is_safe:
-                    print("[*] Безопасно. Установка...")
+                    print("Безопасно. Установка...")
                     subprocess.check_call([
                         sys.executable, "-m", "pip", "install", 
                         "--no-index", f"--find-links={sandbox_temp}", now_package
                     ])
                 else:
-                    print("УСТАНОВКА ЗАБЛОКИРОВАНА.")
+                    print("\nУСТАНОВКА ЗАБЛОКИРОВАНА! ИИ обнаружил угрозу.")
                     sys.exit(1)
             except Exception as error:
                 print(f"Ошибка: {error}")
